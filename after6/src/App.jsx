1f3b7993bef8bc8752129abc5e0c0e6f8c50aa7e
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2025-05-05 20:28:19 -0300

    after6

--- after6/src/App.jsx
+++ after6/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(()=>{
+      fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results); 
+        setLoading(false);
+    })
+    .catch(error => console.error('Error al cargar los personajes:', error));
+  }, []);
+    
+    
+    return (
+      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Personajes de Rick and Morty</h1>
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        <div>
+          {characters.map(character => (
+            <div key={character.id} style={{ margin: '1rem', display: 'inline-block' }}>
+              <img src={character.image} alt={character.name} style={{ width: '150px', height: '150px', borderRadius: '50%' }} />
+              <h3>{character.name}</h3>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App
